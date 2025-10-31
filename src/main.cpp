68d5611c24a50b2cb51eb426e99dcc8b13c5ea1f
Anatoly-E <edush-anatoly@yandex.ru>
2025-10-31 20:51:27 +0300

    Готова программная и аппаратная часть.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
-#include <math.h>
+
+// Настройка системы
+constexpr unsigned long ALARM_DURATION = 3000;  // Длительность звуковой сигнализации (мс)
+constexpr int ALARM_DISTANCE = 150;             // Порог дистанции срабатывания охранной сигнализации (см)
+constexpr unsigned long INTERVAL_SERVO = 20;    // Пауза между шагами серво для плавного движения (мс)
+constexpr unsigned long INTERVAL_SENSOR = 2000; // Интервал опроса датчиков (мс)
 
 // LCD
-#define I2C_ADDR 0x27  // Код I2C устройства
-#define LCD_COLUMNS 16 // Количество колонок
-#define LCD_LINES 2    // Количество строк
+constexpr uint8_t I2C_ADDR = 0x27;  // Код I2C устройства
+constexpr uint8_t LCD_COLUMNS = 16; // Количество колонок
+constexpr uint8_t LCD_LINES = 2;    // Количество строк
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
 byte degree[8] = // Кодируем символ градуса
@@ -23,44 +28,66 @@ byte degree[8] = // Кодируем символ градуса
 };
 
 // DHT Сенсор
-#define DHTPIN 6      // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11 // DHT22 для эмулятора, DHT 11 для реального устройства
+constexpr uint8_t DHTPIN = 6;      // Digital pin connected to the DHT sensor
+constexpr uint8_t DHTTYPE = DHT11; // DHT22 для эмулятора, DHT 11 для реального устройства
 DHT dht(DHTPIN, DHTTYPE);
 
 // Датчик ультразвука
-#define ECHO_PIN 9
-#define TRIG_PIN 8
-#define BUZZER_PIN 7
-const int alarmDistance = 50; // см
-bool alarmActive = false;
+constexpr uint8_t ECHO_PIN = 8;
+constexpr uint8_t TRIG_PIN = 9;
+constexpr uint8_t BUZZER_PIN = 7;
+
+// Параметры для охранной сигнализации
 unsigned long alarmStartTime = 0;
+bool alarmActive = false;
+enum SystemState
+{
+  SYSTEM_ALARM,
+  SYSTEM_OK,
+  SYSTEM_STANDBY
+};
+SystemState sysState = SystemState::SYSTEM_OK;
+
+// Функция возвращает текст по значению состояния
+const char *getSystemStateText(SystemState state)
+{
+  switch (state)
+  {
+  case SystemState::SYSTEM_ALARM:
+    return "Alarm!!!";
+  case SystemState::SYSTEM_OK:
+    return "OFF";
+  case SystemState::SYSTEM_STANDBY:
+    return "Standby";
+  default:
+    return "Unknown";
+  }
+}
 
 // Определяем входы/выходы
-const int BUTTON_PIN = 3; // Пульт
-const int LED_PIN = 13;   // Электрооборудование
-const int GUARD_PIN = 12; // Охранная сигнализация
-const int ALARM_PIN = 11; // Сигнал тревоги
-const int SERVO_PIN = 5;  // Ворота
+constexpr uint8_t BUTTON_PIN = 3; // Пульт
+constexpr uint8_t LED_PIN = 13;   // Электрооборудование
+constexpr uint8_t GUARD_PIN = 12; // Охранная сигнализация
+constexpr uint8_t ALARM_PIN = 11; // Сигнал тревоги
+constexpr uint8_t SERVO_PIN = 5;  // Ворота
 
 // Сервопривод
-int maxAngle = 180;                     // Максимальный угол поворота сервопривода
-int minAngle = 0;                       // Минимальный угол поворота сервопривода
-int step = 1;                           // Шаг поворота
-unsigned long lastMoveTime = 0;         // Счётчик для плавного вращения серво
-const unsigned long intervalServo = 20; // Пуза в мс между шагами серво для плавного движения
+constexpr int maxAngle = 180;   // Максимальный угол поворота сервопривода
+constexpr int minAngle = 0;     // Минимальный угол поворота сервопривода
+constexpr int step = 1;         // Шаг поворота
+unsigned long lastMoveTime = 0; // Счётчик для плавного вращения серво
 Servo gateServo;
 
-unsigned long previousMillisButton = 0;    // для управления таймером опроса Пульта
-unsigned long previousMillisSensor = 0;    // для управления таймером опроса Датчиков
-unsigned long previousMillisServo = 0;     // для управления таймером открытия Ворот
-const unsigned long intervalButton = 1000; // Интервал между опросами пульта
-const long intervalSensor = 2000;          //
-int angle = 0;                             // Угол поворота сервопривода
-bool gateOpen = true;                      // Открыты ворота или закрыты
-bool readyForButton = true;                // кнопка разрешена только в крайних позициях
+// Переменные
+unsigned long previousMillisButton = 0; // для управления таймером опроса Пульта
+unsigned long previousMillisSensor = 0; // для управления таймером опроса Датчиков
+unsigned long previousMillisServo = 0;  // для управления таймером открытия Ворот
+int angle = 0;                          // Угол поворота сервопривода
+bool gateOpen = false;                  // Открыты ворота или закрыты
+bool readyForButton = true;             // кнопка разрешена только в крайних позициях
 
 // Состояния системы
-enum GateState
+enum class GateState
 {
   IDLE_OPEN,
   IDLE_CLOSED,
@@ -70,20 +97,18 @@ enum GateState
   WAITING_CLOSE,
   ALARM
 };
-GateState state = IDLE_OPEN;
-String currentLCDMessage = "";
+GateState gateState = GateState::IDLE_OPEN;
 
 // Антидребезг кнопки
-bool lastButtonState = HIGH;   // предыдущий логический уровень кнопки
-bool stableButtonState = HIGH; // подтверждённое состояние кнопки
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50; // 50 мс — идеально
+bool lastButtonState = HIGH;                 // предыдущий логический уровень кнопки
+bool stableButtonState = HIGH;               // подтверждённое состояние кнопки
+unsigned long lastDebounceTime = 0;          // для опроса кнопки
+constexpr unsigned long DEBOUNCE_DELAY = 50; // 50 мс — идеально
 
-// Счётчик
-volatile unsigned long tickCount = 0;
-float lastTemp = 0;
-float lastHum = 0;
-float lastDist = 0;
+// Отслеживаемые датчики
+float lastTemp = 0.0f; // Температура
+float lastHum = 0.0f;  // Влажность
+float lastDist = 0.0f; // Расстояние
 
 // Опрос датчика температуры
 void readDHTTask()
@@ -95,15 +120,16 @@ void readDHTTask()
   {
     lastTemp = t;
     lastHum = h;
-    Serial.print("Температура: ");
+    Serial.print("Temperature: ");
     Serial.print(t);
     Serial.println(" °C");
-    Serial.print("Влажность: ");
-    Serial.println(h);
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.println("%");
   }
   else
   {
-    Serial.println("[DHT22] Ошибка чтения!");
+    Serial.println("[DHT22] Read error!");
   }
 }
 
@@ -117,34 +143,106 @@ long readDistanceCM()
   digitalWrite(TRIG_PIN, LOW);
 
   long duration = pulseIn(ECHO_PIN, HIGH, 20000); // таймаут ~20ms
-  long distance = duration / 58;
-  return distance;
+  lastDist = duration / 58;
+
+  return lastDist;
 }
 
 // Обновление экрана
-void updateLCD(String text)
+void updateLCD(const String &text, SystemState systemState)
 {
-  if (text != currentLCDMessage)
+
+  static String lastText = "";
+
+  if (text != lastText)
   {
-    currentLCDMessage = text;
+    lastText = text;
     lcd.clear();
+  }
+
+  switch (systemState)
+  {
+  case SYSTEM_OK:
     lcd.setCursor(0, 0);
     lcd.print(text);
     lcd.setCursor(0, 1);
-    lcd.print("T:");
+    lcd.print(F("T:"));
     lcd.print(lastTemp, 0);
-    lcd.print("\1C");
-    lcd.print("H:");
+    lcd.print("\1C H:");
     lcd.print(lastHum, 0);
     lcd.print("%");
-    lcd.print("D:");
+    break;
+
+  case SYSTEM_STANDBY:
+    lcd.setCursor(0, 0);
+    lcd.print(text);
+    lcd.setCursor(0, 1);
+    lcd.print(F("T:"));
+    lcd.print(lastTemp, 0);
+    lcd.print("\1C H:");
+    lcd.print(lastHum, 0);
+    lcd.print("%");
+    break;
+
+  case SYSTEM_ALARM:
+    lcd.setCursor(0, 0);
+    lcd.print(text);
+    lcd.setCursor(0, 1);
+    lcd.print(F("Distance: "));
     lcd.print(lastDist, 0);
-    lcd.print("cm");
+    lcd.print("sm");
+    break;
   }
+}
+
+// Охранная сигнализация
+void checkAlarm()
+{
+  if (gateState != GateState::IDLE_CLOSED)
+  {
+    sysState = SYSTEM_OK;
+    digitalWrite(ALARM_PIN, LOW);
+    noTone(BUZZER_PIN);
+    alarmActive = false;
+    return;
+  }
+
+  sysState = SYSTEM_STANDBY;
+  readDistanceCM();
+
+  if (lastDist > 0 && lastDist < ALARM_DISTANCE)
+  {
+    sysState = SYSTEM_ALARM;
+    if (!alarmActive)
+    {
+      alarmActive = true;
+      alarmStartTime = millis();
+      digitalWrite(ALARM_PIN, HIGH);
+      tone(BUZZER_PIN, 2000);
+    }
+    updateLCD(F("Alarm!"), sysState);
+  }
+  else if (alarmActive && (millis() - alarmStartTime >= ALARM_DURATION))
+  {
+    noTone(BUZZER_PIN);
+    alarmActive = false;
+    sysState = SYSTEM_STANDBY;
+  }
+
+  Serial.print(F("State: "));
+  Serial.println(getSystemStateText(sysState));
+  Serial.print(F("Dist: "));
+  Serial.print(lastDist);
+  Serial.println(F("cm"));
 }
 
 void setup()
 {
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(GUARD_PIN, LOW);
+  digitalWrite(ALARM_PIN, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
+
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
   pinMode(GUARD_PIN, OUTPUT);
@@ -152,30 +250,39 @@ void setup()
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
-  digitalWrite(BUZZER_PIN, LOW);
 
   // Сервопривод
   gateServo.attach(SERVO_PIN);
-  gateServo.write(angle); // Стартовое положение ворота закрыты
-
-  digitalWrite(LED_PIN, LOW);
+  gateServo.write(minAngle);
+  gateState = GateState::IDLE_OPEN;
 
   // Запись в Serial Port
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Инициализация LCD
   lcd.init();
   lcd.backlight();
   lcd.createChar(1, degree); // Создаем символ под номером 1
-  updateLCD("System ready!");
 
   // Датчик температуры и влажности
   dht.begin();
+
+  // Проверка УЗ и установка начального значения
+  readDHTTask();
+  readDistanceCM();
+
+  // Инициализация - ворота открыты
+  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(GUARD_PIN, LOW);
+  digitalWrite(ALARM_PIN, LOW);
+  gateServo.write(minAngle);
+
+  updateLCD("System ready!", SYSTEM_OK);
 }
 
 void loop()
 {
-  // АНТИДРЕБЕЗГ КНОПКИ
+  // Антидребезг кнопки
   bool rawButtonState = digitalRead(BUTTON_PIN);
 
   if (rawButtonState != lastButtonState)
@@ -185,7 +292,7 @@ void loop()
 
   lastButtonState = rawButtonState;
 
-  if ((millis() - lastDebounceTime) > debounceDelay)
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY)
   {
     if (rawButtonState != stableButtonState)
     {
@@ -202,15 +309,15 @@ void loop()
         else
         {
           // Кнопка нажата во время движения → показываем статус ожидания
-          if (state == CLOSING && state != WAITING_CLOSE)
+          if (gateState == GateState::CLOSING && gateState != GateState::WAITING_CLOSE)
           {
-            state = WAITING_CLOSE;
-            updateLCD("Wait! Closing...");
+            gateState = GateState::WAITING_CLOSE;
+            updateLCD("Wait! Closing...", SYSTEM_OK);
           }
-          else if (state == OPENING && state != WAITING_OPEN)
+          else if (gateState == GateState::OPENING && gateState != GateState::WAITING_OPEN)
           {
-            state = WAITING_OPEN;
-            updateLCD("Wait! Opening...");
+            gateState = GateState::WAITING_OPEN;
+            updateLCD("Wait! Opening...", SYSTEM_OK);
           }
         }
       }
@@ -218,7 +325,7 @@ void loop()
   }
 
   // ДВИЖЕНИЕ СЕРВО
-  if (millis() - lastMoveTime >= intervalServo)
+  if (millis() - lastMoveTime >= INTERVAL_SERVO)
   {
     lastMoveTime = millis();
 
@@ -226,23 +333,24 @@ void loop()
     {
       if (angle < maxAngle)
       {
-        if (state != CLOSING && state != WAITING_CLOSE)
+        // Процесс закрытия
+        if (gateState != GateState::CLOSING && gateState != GateState::WAITING_CLOSE)
         {
-          state = CLOSING;
-          updateLCD("Closing...");
+          gateState = GateState::CLOSING;
+          updateLCD("Closing...", SYSTEM_OK);
         }
         angle++;
         gateServo.write(angle);
       }
       else
       {
-        if (state != IDLE_CLOSED)
+        // Закрыты
+        if (gateState != GateState::IDLE_CLOSED)
         {
           digitalWrite(LED_PIN, LOW);
           digitalWrite(GUARD_PIN, HIGH);
-          digitalWrite(ALARM_PIN, HIGH);
-          state = IDLE_CLOSED;
-          updateLCD("Closed!");
+          gateState = GateState::IDLE_CLOSED;
+          updateLCD("Closed!", SYSTEM_OK);
         }
         readyForButton = true;
       }
@@ -251,34 +359,36 @@ void loop()
     {
       if (angle > minAngle)
       {
-        if (state != OPENING && state != WAITING_OPEN)
+        // Процесс открытия
+        if (gateState != GateState::OPENING && gateState != GateState::WAITING_OPEN)
         {
           digitalWrite(LED_PIN, HIGH);
           digitalWrite(GUARD_PIN, LOW);
-          state = OPENING;
-          updateLCD("Opening...");
+
+          gateState = GateState::OPENING;
+          updateLCD("Opening...", SYSTEM_OK);
         }
         angle--;
         gateServo.write(angle);
       }
       else
       {
-        if (state != IDLE_OPEN)
+        // Открыты
+        if (gateState != GateState::IDLE_OPEN)
         {
-          state = IDLE_OPEN;
-          updateLCD("Opened!");
+          gateState = GateState::IDLE_OPEN;
+          updateLCD("Opened!", SYSTEM_OK);
         }
         readyForButton = true;
       }
     }
   }
 
-  // ОПРОС ДАТЧИКОВ
-  unsigned long currentMillisSensor = millis();
-  if (currentMillisSensor - previousMillisSensor >= intervalSensor)
+  // Опрос датчиков и охранной сигнализации
+  if (millis() - previousMillisSensor >= INTERVAL_SENSOR)
   {
-    previousMillisSensor = currentMillisSensor;
+    previousMillisSensor = millis();
     readDHTTask();
-    readDistanceCM();
+    checkAlarm();
   }
 }
